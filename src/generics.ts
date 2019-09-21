6cb559de5ce7af66f2eaf6dfe2f1d4a294e6857c
Philipp Muens <raute1337@gmx.de>
2019-09-21 12:10:18 +0200

    Update generics code to use new logging util

--- src/generics.ts
+++ src/generics.ts
@@ -1,3 +1,5 @@
+import { println } from './utils/logging'
+
 // our lists we want to `map` over
 const someNumbers = [1, 2, 3, 4, 5, 6]
 const someStrings = ['Alice', 'Bob', 'John', 'Alex', 'Pete']
@@ -9,17 +11,16 @@ const startsWithA = (str: string): boolean => str.toLowerCase().charAt(0) === 'a
 const wrapStr = (str: string): string => `<blink>${str}</blink>`
 const numToString = (num: number): string => `${num}`
 
-// some convenient variables / functions
+// convenience variables
 let result
-const log = (msg: string) => console.log(`${msg}\n`)
 
 // --------------- Filter ---------------
 // using the built-in `filter` functionality
 result = someNumbers.filter(isEven)
-log(`built-in filter with numbers: ${result}`)
+println(`built-in filter with numbers: ${result}`)
 
 result = someStrings.filter(startsWithA)
-log(`built-in filter with strings: ${result}`)
+println(`built-in filter with strings: ${result}`)
 
 // implementing our own `filter` function
 function filter1(xs: number[], func: (x: number) => boolean): number[] {
@@ -33,11 +34,11 @@ function filter1(xs: number[], func: (x: number) => boolean): number[] {
 }
 
 result = filter1(someNumbers, isEven)
-log(`filter1 with numbers: ${result}`)
+println(`filter1 with numbers: ${result}`)
 
 // NOTE: we cannot use our `filter1` on strings since `filter1` can only deal with numbers --> Solution: Use Generics!
 // result = filter1(someStrings, startsWithA)
-// log(`filter1 with strings: ${result}`)
+// println(`filter1 with strings: ${result}`)
 
 // the generic version of our `filter` function which can deal with any type
 function filter2<T>(xs: T[], func: (x: T) => boolean): T[] {
@@ -51,18 +52,18 @@ function filter2<T>(xs: T[], func: (x: T) => boolean): T[] {
 }
 
 result = filter2(someNumbers, isEven)
-log(`filter2 with numbers: ${result}`)
+println(`filter2 with numbers: ${result}`)
 
 result = filter2(someStrings, startsWithA)
-log(`filter2 with strings: ${result}`)
+println(`filter2 with strings: ${result}`)
 
 // --------------- Map ---------------
 // using the built-in `map` functionality
 result = someNumbers.map(add2)
-log(`built-in map with numbers: ${result}`)
+println(`built-in map with numbers: ${result}`)
 
 result = someStrings.map(wrapStr)
-log(`built-in map with strings: ${result}`)
+println(`built-in map with strings: ${result}`)
 
 // implementing our own `map` function
 function map1(xs: number[], func: (x: number) => number): number[] {
@@ -74,11 +75,11 @@ function map1(xs: number[], func: (x: number) => number): number[] {
 }
 
 result = map1(someNumbers, add2)
-log(`map1 with numbers: ${result}`)
+println(`map1 with numbers: ${result}`)
 
 // NOTE: we cannot use our `map1` on strings since `map1` can only deal with numbers --> Solution: Use Generics!
 // result = map1(someStrings, wrapStr)
-// log(`map1 with strings: ${result}`)
+// println(`map1 with strings: ${result}`)
 
 // the generic version of our `map` function which can deal with any type
 function map2<A, B>(xs: A[], func: (x: A) => B): B[] {
@@ -90,11 +91,11 @@ function map2<A, B>(xs: A[], func: (x: A) => B): B[] {
 }
 
 result = map2(someNumbers, add2)
-log(`map2 with numbers: ${result}`)
+println(`map2 with numbers: ${result}`)
 
 result = map2(someStrings, wrapStr)
-log(`map2 with strings: ${result}`)
+println(`map2 with strings: ${result}`)
 
 // proof that we can turn the input of type A into a result of type B
 result = map2(someNumbers, numToString)
-log(`map2 with a list of numbers turned into a list of strings: ${result}`)
+println(`map2 with a list of numbers turned into a list of strings: ${result}`)
